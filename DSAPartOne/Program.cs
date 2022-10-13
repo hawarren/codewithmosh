1793d2956220c039cc1d921508872159cd4bacf4
Hanif Warren <hanif.warren@gmail.com>
2022-10-12 22:11:15 -0400

    Lesson 3 Working with Arrays

---
+++ DSAPartOne/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DSAPartOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // int[] numbers = new int[3];
+            int[] numbers = {10,20,30 };
+            // numbers[0] = 10;
+            // numbers[1] = 20;
+            // numbers[2] = 30;
+            foreach (var item in numbers){
+
+            Console.WriteLine(item.ToString());
+            }
+        }
+        
+    }
+}
