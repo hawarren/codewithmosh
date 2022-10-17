cf19191eb355429897e7db55978b85961dc63d4a
Hanif Warren <hanif.warren@gmail.com>
2022-10-16 23:20:45 -0400

    7 - solution- removeAt() refactored removeAt code to repack the array after removing the slot. Fixed  resize() bug causing array to delete some items when resizing

--- DSAPartOne/Program.cs
+++ DSAPartOne/Program.cs
@@ -13,10 +13,13 @@ namespace DSAPartOne
             myArray.insert(20);
             myArray.insert(30);
             myArray.insert(40);
-            //myArray.insert(50);
-            //myArray.removeAt(3);
+            myArray.print();
+            myArray.insert(50);
+            myArray.removeAt(3);
             Console.WriteLine("Index of 10 is {0}", myArray.indexOf(10));
             Console.WriteLine("Index of 100 is {0}", myArray.indexOf(100));
+            myArray.print();
+            myArray.removeAt(8);
             myArray.print();
 
 
@@ -35,29 +38,36 @@ namespace DSAPartOne
             internalArray = new int[initialSize];
         }
         public void insert(int newItem)
-        {            
+        {
             if (internalArray.Length == count)
             {
                 resize();
             }
             internalArray[count++] = newItem; //add item to array at last position, then increment count
-            Console.WriteLine("{0} was added to the array", newItem);            
+            Console.WriteLine("{0} was added to the array", newItem);
+            print();
         }
-        public int removeAt(int indexToDelete)
+        public void removeAt(int indexToDelete)
         {
-            if (internalArray.Length - 1 > indexToDelete)
+            //validate the indexToDelete
+            if (indexToDelete < 0 || indexToDelete >= count)
             {
-                var tempNumber = internalArray[indexToDelete];
-                internalArray[indexToDelete] = 0;
-                count--;
-                Console.WriteLine("{0} was deleted from position {1}", tempNumber, indexToDelete);
-                return tempNumber;
+                throw new ArgumentOutOfRangeException(String.Format("{0} is not a valid index", indexToDelete));
             }
-            else
-            {
-                Console.WriteLine("Please choose value within index range");
-                return -1;
+            //shift items left to fill the hole
+            //[10,20,30,40]
+            //index: 1
+            //1 <- 2
+            //2 <-3
+            var deletedNumber = internalArray[indexToDelete];
+            for (var i = indexToDelete; i < count; i++)
+            {               
+
+                internalArray[i] = internalArray[i + 1];
             }
+            count--; //decrement the count since we removed an entry
+            Console.WriteLine("{0} was deleted from position {1}", deletedNumber, indexToDelete);
+            return;
         }
         public int indexOf(int numToCheck)
         {
@@ -74,26 +84,27 @@ namespace DSAPartOne
         void resize()
         {
             int[] bigInternalArray = new int[internalArray.Length * 2];
-                       
-            for (var i = 0; i < count; i++ )
+
+            for (var i = 0; i < count; i++)
             {
                 //put item in new array
                 bigInternalArray[i] = internalArray[i];
-               
-                //update size of big (new) array
-                internalArray = bigInternalArray;
-                Console.WriteLine("Array using {0} of {1} slots", count, internalArray.Length);
-            }           
+            }
+
+            //replace old array with bigger array
+            internalArray = bigInternalArray;
+            Console.WriteLine("Array using {0} of {1} slots", count, internalArray.Length);
+
+            Console.WriteLine("New array size is {0}", internalArray.Length);
         }
         public void print()
         {
-            List<int> arrayList = new List<int>();
-            for (var i = 0; i < internalArray.Length; i++)
+            var sb = new System.Text.StringBuilder();
+            for (var i = 0; i < count; i++)
             {
-                arrayList.Add(internalArray[i]);
-                Console.WriteLine(internalArray[i]);
+                sb.AppendFormat("..{0}", internalArray[i].ToString());
             }
-            Console.WriteLine(arrayList.ToString());
+            Console.WriteLine(sb.ToString());
         }
 
     }
